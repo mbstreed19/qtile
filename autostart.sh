90a6944e8214bced3fb50d6064fac978c49ff40c
Mikey Streed <mbstreed19@outlook.com>
2023-12-23 07:39:00 -0600

    backing up my files before partition change

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+feh --bg-scale ~/Pictures/Cassini-Release-Images/livewall.png
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
--- modules/eos-hooks.py
+++ modules/eos-hooks.py
@@ -1,16 +1,19 @@
+
 from libqtile import hook
 import subprocess
 import os
-#from libqtile.utils import send_notification
+from libqtile.utils import send_notification
+
 
 @hook.subscribe.startup
 def autostart():
-    home = os.path.expanduser("~/.config/qtile/")
-    subprocess.call([home + "autostart.sh"])
-    # send_notification("running autostart")
+    home = os.path.expanduser("~")
+    subprocess.call([home + "/.config/qtile/autostart"])
+    send_notification("running autostart")
+
 
 @hook.subscribe.startup_once
 def screenlayout():
     home = os.path.expanduser("~")
     subprocess.call([home + "/.screenlayout/screenlayout.sh"])
-   # send_notification("setting up screenlayout")
+    send_notification("setting up screenlayout")
